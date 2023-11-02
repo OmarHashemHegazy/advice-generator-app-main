1f5901ff66b8be64e8d1dade9a94b3380eb95b0e
Omar Hashem Mostafa <94738353+OmarHashemHegazy@users.noreply.github.com>
2023-11-02 11:08:19 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const adviceId = document.querySelector('#adviceId');
+const adviceText = document.querySelector('#adviceText');
+const btn = document.querySelector('#btn');
+
+console.log(adviceId)
+console.log(adviceText)
+console.log(btn)
+
+
+
+function getAdvice(){
+    fetch('https://api.adviceslip.com/advice').then(response =>{
+        return response.json();
+        
+    }).then(adviceData =>{
+        const adviceNum = adviceData.slip.id;
+        const advice = adviceData.slip.advice;
+
+
+        adviceId.textContent = adviceNum;
+        adviceText.innerHTML = `<p>${advice}</p>`;
+    }).catch(error =>{
+        console.log(error);
+    })
+}
+
+btn.addEventListener('click', function(){
+    getAdvice();
+})
+
+
